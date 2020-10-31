89c6fa7f0d23ab2e4d8f9870c093bf2653a7b75a
Sénant Trévor <mouchet19@mbp-de-senant.home>
2020-10-31 14:37:45 +0100

    Initial

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("Page chargée");
+
+  $.querySelector("#contact-form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const data = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#email").value,
+      subject: $.querySelector("#subject").value,
+      description: $.querySelector("#description").value,
+    };
+    console.log(data);
+
+    const response = await axios.post("https://260652ce1274.ngrok.io", data);
+    if (response.status === 200) {
+      alert("Un mail vous a été envoyé");
+    } else {
+      alert("Une erreur est survenue");
+    }
+  });
+});
